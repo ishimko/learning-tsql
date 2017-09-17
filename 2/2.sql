71776cdac6f20e6f7d49f96b70a9ab8b5ca15b17
Ivan Shimko <vana.shimko@gmail.com>
2017-09-17 22:11:06 +0300

    Add variant 2 to lab 2, task 2

--- 2/2.sql
+++ 2/2.sql
@@ -18,8 +18,7 @@ CREATE TABLE [dbo].[Person](
 	[EmailPromotion] [int] NOT NULL,
 	[ModifiedDate] [datetime] NOT NULL);
 
-ALTER TABLE [dbo].[Person] ADD ID BIGINT IDENTITY(10, 10);
-ALTER TABLE [dbo].[Person] ADD CONSTRAINT PK_ID PRIMARY KEY CLUSTERED (ID);
+ALTER TABLE [dbo].[Person] ADD ID BIGINT IDENTITY(10, 10) PRIMARY KEY;
 
 ALTER TABLE [dbo].[Person] ADD CONSTRAINT check_title CHECK ([Title] IN ('Mr.', 'Ms.'));
 
@@ -36,7 +35,7 @@ INSERT INTO [dbo].[Person]
            ,[Suffix]
            ,[EmailPromotion]
            ,[ModifiedDate])
-SELECT [p].[BusinessEntityID]
+SELECT [e].[BusinessEntityID]
            ,[PersonType]
            ,[NameStyle]
            ,[Title]
@@ -45,7 +44,7 @@ SELECT [p].[BusinessEntityID]
            ,[LastName]
            ,[Suffix]
            ,[EmailPromotion]
-           ,[p].[ModifiedDate]
+           ,[e].[ModifiedDate]
 FROM [Person].[Person] AS [p]
 JOIN [HumanResources].[Employee] AS [e] ON [p].[BusinessEntityID] = [e].[BusinessEntityID]
 JOIN [HumanResources].[EmployeeDepartmentHistory] AS [edh] ON [e].[BusinessEntityID] = [edh].[BusinessEntityID]
@@ -53,3 +52,42 @@ JOIN [HumanResources].[Department] AS [d] ON [edh].[DepartmentID] = [d].[Departm
 WHERE [d].[Name] != 'Executive' AND [edh].[EndDate] IS NULL
 
 ALTER TABLE [dbo].[Person] ALTER COLUMN [Suffix] nvarchar(5)
+
+
+-- 2
+
+
+DROP TABLE [dbo].[PersonPhone]
+
+
+CREATE TABLE [dbo].[PersonPhone](
+	[BusinessEntityID] [int] NOT NULL,
+	[PhoneNumber] [dbo].[Phone] NOT NULL,
+	[PhoneNumberTypeID] [int] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL)
+
+ALTER TABLE [dbo].[PersonPhone] ADD ID BIGINT IDENTITY(2, 2) UNIQUE;
+
+ALTER TABLE [dbo].[PersonPhone] ADD CONSTRAINT phone_only_numbers CHECK ([PhoneNumber] LIKE '%[^A-Z]%')
+
+ALTER TABLE [dbo].[PersonPhone] ADD CONSTRAINT default_type_id DEFAULT 1 FOR [PhoneNumberTypeID];
+
+
+INSERT INTO [dbo].[PersonPhone]
+           ([BusinessEntityID]
+           ,[PhoneNumber]
+           ,[PhoneNumberTypeID]
+           ,[ModifiedDate])
+SELECT [e].[BusinessEntityID]
+      ,[PhoneNumber]
+      ,[PhoneNumberTypeID]
+      ,[e].[ModifiedDate]
+FROM [Person].[PersonPhone] AS [ph]
+JOIN [HumanResources].[Employee] AS [e] ON [ph].[BusinessEntityID] = [e].[BusinessEntityID]
+JOIN [HumanResources].[EmployeeDepartmentHistory] AS [edh] ON [edh].[BusinessEntityID] = [e].[BusinessEntityID]
+
+WHERE [ph].[PhoneNumber] NOT LIKE '%[()]%'
+	  AND [edh].[EndDate] IS NULL
+	  AND [e].[HireDate] = [edh].[StartDate];
+
+ALTER TABLE [dbo].[PersonPhone] ALTER COLUMN [PhoneNumber] [dbo].[Phone] NULL;
