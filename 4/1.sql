83ad2d2f8e289d434073d6f19e2ab2e8b1cd0696
Ivan Shimko <vana.shimko@gmail.com>
2017-09-26 22:37:45 +0300

    Add task 4.1

---
+++ 4/1.sql
@@ -0,0 +1,74 @@
+USE AdventureWorks2012;
+GO
+
+DROP TABLE [Production].[LocationHost];
+GO
+
+DROP TRIGGER [Production].[Trigger_Location_After];
+GO
+
+DROP VIEW [Production].[View_Location];
+GO
+
+DELETE FROM [Production].[Location]
+WHERE [Name] = 'Test Location';
+
+-- a
+CREATE TABLE [Production].[LocationHost] (
+	ID INT IDENTITY(1, 1) PRIMARY KEY,
+	Action CHAR(6) NOT NULL CHECK (Action IN('insert', 'update', 'delete')),
+    ModifiedDate DATETIME NOT NULL,
+    SourceID INT NOT NULL,
+    UserName VARCHAR(50) NOT NULL
+);
+GO
+
+
+-- b
+CREATE TRIGGER [Production].[Trigger_Location_After]
+ON [Production].[Location]
+AFTER INSERT, UPDATE, DELETE AS 
+    INSERT INTO [Production].[LocationHost](Action, ModifiedDate, SourceID, UserName) 
+    SELECT
+      CASE WHEN inserted.LocationID IS NULL THEN 'delete'
+           WHEN  deleted.LocationID IS NULL THEN 'insert'
+		   ELSE 'update'
+      END                                                   AS Action,
+      GETDATE()                                             AS ModifiedDate,
+	  COALESCE(inserted.LocationID, deleted.LocationID) AS SourceID,
+      USER_NAME()                                           AS UserName
+    FROM inserted FULL OUTER JOIN deleted
+    ON inserted.LocationID = deleted.LocationID
+GO
+
+
+-- c
+CREATE VIEW [Production].[View_Location] AS SELECT * FROM [Production].[Location]
+GO
+
+
+--d
+INSERT INTO [Production].[View_Location]
+           ([Name]
+           ,[CostRate]
+           ,[Availability]
+           ,[ModifiedDate])
+     VALUES
+           ('Test Location'
+           ,'123' 
+           ,'1.1' 
+           ,GETDATE())
+GO
+
+
+
+UPDATE [Production].[Location]
+   SET [Name] = 'Test location 1'
+ WHERE [Name] = 'Test location'
+GO
+
+DELETE FROM [Production].[Location]
+WHERE [Name] = 'Test location 1';
+
+SELECT * FROM [Production].[LocationHost];
+
