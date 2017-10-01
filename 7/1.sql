fe21c3e435273a657b730e112353adbd4110774c
Ivan Shimko <vana.shimko@gmail.com>
2017-10-01 12:17:08 +0300

    Task 7: code-style fixes

--- 7/1.sql
+++ 7/1.sql
@@ -2,23 +2,24 @@ USE [AdventureWorks2012]
 GO
 
 -- 1
-SELECT
-  [BusinessEntityID] AS '@ID',
-  [NationalIDNumber],
-  [JobTitle]
-FROM [HumanResources].[Employee]
-FOR XML PATH ('Employee'), ROOT ('Employees');
 
 
-IF OBJECT_ID('[dbo].[#EmployeeTemporary]', 'T') IS NOT NULL
+IF OBJECT_ID('[tempdb].[dbo].[#EmployeeTemporary]', 'U') IS NOT NULL
   DROP TABLE [dbo].[#EmployeeTemporary];
 GO
 
+SELECT
+	[BusinessEntityID] AS '@ID',
+	[NationalIDNumber],
+	[JobTitle]
+FROM [HumanResources].[Employee]
+FOR XML PATH ('Employee'), ROOT ('Employees')
+
 CREATE TABLE [dbo].[#EmployeeTemporary]
 (
-  BusinessEntityID INT,
-  NationalIDNumber NVARCHAR(100),
-  JobTitle         NVARCHAR(100)
+  [BusinessEntityID] INT,
+  [NationalIDNumber] NVARCHAR(15),
+  [JobTitle]         NVARCHAR(50)
 );
 
 DECLARE @xml_var_Employee XML =
@@ -31,17 +32,20 @@ DECLARE @xml_var_Employee XML =
   FOR XML PATH ('Employee'), ROOT ('Employees')
 );
 
-INSERT INTO [dbo].[#EmployeeTemporary] ([BusinessEntityID], [NationalIDNumber], [JobTitle])
-  SELECT
-    xc.value('@ID', 'int'),
-    xc.value('NationalIDNumber[1]', 'nvarchar(100)'),
-    xc.value('JobTitle[1]', 'nvarchar(100)')
-  FROM @xml_var_Employee.nodes('//Employee') XmlData(xc);
 
-SELECT *
-FROM [dbo].[#EmployeeTemporary];
+INSERT INTO [dbo].[#EmployeeTemporary] ([BusinessEntityID], [NationalIDNumber], [JobTitle])
+	SELECT
+		xc.value('@ID', 'int'),
+		xc.value('NationalIDNumber[1]', 'nvarchar(15)'),
+		xc.value('JobTitle[1]', 'nvarchar(50)')
+	FROM @xml_var_Employee.nodes('//Employee') AS XmlData(xc);
+
+SELECT * FROM [dbo].[#EmployeeTemporary];
+
 
 -- 2
+
+
 SELECT
   [ProductID] AS '@ID',
   [Name],
@@ -57,13 +61,14 @@ GO
 CREATE PROCEDURE [dbo].[GetProductsFromXml]
     @xml_var_P XML
 AS
-  BEGIN
-    SELECT
-      xc.value('@ID', 'int')                        AS ID,
-      xc.value('Name[1]', 'nvarchar(100)')          AS NAME,
-      xc.value('ProductNumber[1]', 'nvarchar(100)') AS ProductNumber
-    FROM @xml_var_P.nodes('//Product') XmlData(xc)
-  END;
+BEGIN
+	SELECT
+		xc.value('@ID', 'int')                        AS ID,
+		xc.value('Name[1]', 'nvarchar(50)')           AS NAME,
+		xc.value('ProductNumber[1]', 'nvarchar(25)')  AS ProductNumber
+	FROM @xml_var_P.nodes('//Product') AS XmlData(xc)
+END;
+GO
 
 DECLARE @xml_var_Product XML =
 (
@@ -77,7 +82,10 @@ DECLARE @xml_var_Product XML =
 
 EXECUTE [dbo].[GetProductsFromXml] @xml_var_Product;
 
+
 -- 3
+
+
 SELECT
   [BusinessEntityID] AS 'ID',
   [FirstName],
@@ -86,15 +94,15 @@ FROM [Person].[Person]
 FOR XML PATH ('Person'), ROOT ('Persons');
 
 
-IF OBJECT_ID('[dbo].[#PersonTemporary]', 'T') IS NOT NULL
+IF OBJECT_ID('[tempdb].[dbo].[#PersonTemporary]', 'U') IS NOT NULL
   DROP TABLE [dbo].[#PersonTemporary];
 GO
 
 CREATE TABLE [dbo].[#PersonTemporary]
 (
-  BusinessEntityID INT,
-  FirstName        NVARCHAR(100),
-  LastName         NVARCHAR(100)
+  [BusinessEntityID] INT,
+  [FirstName]       NVARCHAR(50),
+  [LastName]         NVARCHAR(50)
 );
 
 DECLARE @xml_var_Person XML =
@@ -108,18 +116,42 @@ DECLARE @xml_var_Person XML =
 );
 
 INSERT INTO [dbo].[#PersonTemporary] ([BusinessEntityID], [FirstName], [LastName])
-  SELECT
-    xc.value('ID[1]', 'int'),
-    xc.value('FirstName[1]', 'nvarchar(100)'),
-    xc.value('LastName[1]', 'nvarchar(100)')
-  FROM @xml_var_Person.nodes('//Person') XmlData(xc);
+	SELECT
+		xc.value('ID[1]', 'int'),
+		xc.value('FirstName[1]', 'nvarchar(50)'),
+		xc.value('LastName[1]', 'nvarchar(50)')
+	FROM @xml_var_Person.nodes('//Person') AS XmlData(xc);
 
-SELECT *
-FROM [dbo].[#PersonTemporary];
+SELECT * FROM [dbo].[#PersonTemporary];
 
--- 4 !
-DECLARE @xml_var_Vendor XML
-SET @xml_var_Vendor =
+
+-- 4
+
+
+SELECT
+	[BusinessEntityID] AS 'ID',
+	[Name],
+	[AccountNumber]
+FROM [Purchasing].[Vendor]
+FOR XML PATH ('Vendor'), ROOT ('Vendors')
+
+IF OBJECT_ID('[dbo].[GetVendorsFromXml]', 'P') IS NOT NULL
+  DROP PROCEDURE [dbo].[GetVendorsFromXml];
+GO
+
+CREATE PROCEDURE [dbo].[GetVendorsFromXml]
+    @xml_var_V XML
+AS
+BEGIN	
+	SELECT
+		xc.value('ID[1]', 'int')						AS ID,
+		xc.value('Name[1]', 'nvarchar(50)')			    AS Name,
+		xc.value('AccountNumber[1]', 'nvarchar(15)')	AS AccountNumber
+	FROM @xml_var_V.nodes('//Vendor') AS XmlData(xc)
+END;
+GO
+
+DECLARE @xml_var_Vendor XML =
 (
   SELECT
     [BusinessEntityID] AS 'ID',
@@ -129,26 +161,23 @@ SET @xml_var_Vendor =
   FOR XML PATH ('Vendor'), ROOT ('Vendors')
 );
 
-CREATE PROCEDURE ShowVendorXml
-    @xml_var_V XML
-AS
-  BEGIN
-    SELECT
-      xc.value('ID[1]', 'int')                      AS ID,
-      xc.value('Name[1]', 'nvarchar(100)')          AS NAME,
-      xc.value('AccountNumber[1]', 'nvarchar(100)') AS AccountNumber
-    FROM @xml_var_V.nodes('//Vendor') XmlData(xc)
-  END;
+EXECUTE [GetVendorsFromXml] @xml_var_Vendor;
 
-EXECUTE [ShowVendorXml] @xml_var_Vendor;
 
-DROP PROCEDURE ShowVendorXml;
+-- 5
 
--------------
---lab_7_option_5
+SELECT
+	[LocationID] AS '@ID',
+	[Name]       AS '@Name',
+	[CostRate]   AS '@Cost'
+FROM [Production].[Location]
+FOR XML PATH ('Location'), ROOT ('Locations')
 
-DECLARE @xml_var_Location XML
-SET @xml_var_Location =
+IF OBJECT_ID('[tempdb].[dbo].[#LocationTmp]', 'U') IS NOT NULL
+	DROP TABLE [dbo].[#LocationTmp]
+GO
+
+DECLARE @xml_var_Location XML =
 (
   SELECT
     [LocationID] AS '@ID',
@@ -160,25 +189,40 @@ SET @xml_var_Location =
 
 CREATE TABLE #LocationTmp
 (
-  LocationID INT,
-  Name       NVARCHAR(100),
-  CostRate   SMALLMONEY
+  [LocationID] INT,
+  [Name]       NVARCHAR(50),
+  [CostRate]   SMALLMONEY
 );
 
-INSERT INTO #LocationTmp ([LocationID], [Name], [CostRate])
+INSERT INTO [dbo].[#LocationTmp] ([LocationID], [Name], [CostRate])
   SELECT
     xc.value('@ID', 'int'),
     xc.value('@Name', 'nvarchar(100)'),
     xc.value('@Cost', 'smallmoney')
-  FROM @xml_var_Location.nodes('//Location') XmlData(xc);
+  FROM @xml_var_Location.nodes('//Location') AS XmlData(xc);
 
-DROP TABLE #LocationTmp;
+SELECT * FROM [dbo].[#LocationTmp];
 
--------------
---lab_7_option_6
 
-DECLARE @xml_var_CreditCard XML
-SET @xml_var_CreditCard =
+-- 6
+
+IF OBJECT_ID('[dbo].[GetCreditCardsFromXml]', 'P') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetCreditCardsFromXml];
+GO
+
+CREATE PROCEDURE [dbo].[GetCreditCardsFromXml]
+    @xml_var_CC XML
+AS
+BEGIN
+	SELECT
+		xc.value('@ID', 'int')               AS ID,
+		xc.value('@Type', 'nvarchar(100)')   AS CardType,
+		xc.value('@Number', 'nvarchar(100)') AS CardNumber
+	FROM @xml_var_CC.nodes('//Card') XmlData(xc)
+END;
+GO
+
+DECLARE @xml_var_CreditCard XML =
 (
   SELECT
     [CreditCardID] AS '@ID',
@@ -188,142 +232,173 @@ SET @xml_var_CreditCard =
   FOR XML PATH ('Card'), ROOT ('CreditCards')
 );
 
-CREATE PROCEDURE ShowCreditCardXml
-    @xml_var_CC XML
-AS
-  BEGIN
-    SELECT
-      xc.value('@ID', 'int')               AS ID,
-      xc.value('@Type', 'nvarchar(100)')   AS CardType,
-      xc.value('@Number', 'nvarchar(100)') AS CardNumber
-    FROM @xml_var_CC.nodes('//Card') XmlData(xc)
-  END;
+EXECUTE [dbo].[GetCreditCardsFromXml] @xml_var_CreditCard;
 
-EXECUTE [ShowCreditCardXml] @xml_var_CreditCard;
 
-DROP PROCEDURE ShowCreditCardXml;
+-- 7
 
--------------
---lab_7_option_7
 
-DECLARE @xml_var_Product XML
-SET @xml_var_Product =
+SELECT
+	[p].[ProductID]      AS '@ID',
+	[p].[Name]           AS 'Name',
+	[p].[ProductModelID] AS 'Model/@ID',
+	[pm].[Name]          AS 'Model/Name'
+FROM [Production].[Product] AS [p]
+JOIN [Production].[ProductModel] AS [pm]
+    ON P.[ProductModelID] = PM.[ProductModelID]
+FOR XML PATH ('Product'), ROOT ('Products')
+
+IF OBJECT_ID('[tempdb].[dbo].[#ProductTmp]', 'U') IS NOT NULL
+	DROP TABLE [dbo].[#ProductTmp]
+GO
+
+CREATE TABLE [dbo].[#ProductTmp]
+(
+  [ProductID]        INT,
+  [ProductName]      NVARCHAR(50),
+  [ProductModelID]   INT,
+  [ProductModelName] NVARCHAR(50)
+);
+
+DECLARE @xml_var_Product XML =
 (
   SELECT
-    P.[ProductID]      AS '@ID',
-    P.[Name]           AS 'Name',
-    P.[ProductModelID] AS 'Model/@ID',
-    PM.[Name]          AS 'Model/Name'
-  FROM [Production].[Product] AS P
-    JOIN [Production].[ProductModel] AS PM
+    [p].[ProductID]      AS '@ID',
+    [p].[Name]           AS 'Name',
+    [p].[ProductModelID] AS 'Model/@ID',
+    [pm].[Name]          AS 'Model/Name'
+  FROM [Production].[Product] AS [p]
+    JOIN [Production].[ProductModel] AS [pm]
       ON P.[ProductModelID] = PM.[ProductModelID]
   FOR XML PATH ('Product'), ROOT ('Products')
 );
 
-CREATE TABLE #ProductTmp
-(
-  ProductID        INT,
-  ProductName      NVARCHAR(100),
-  ProductModelID   INT,
-  ProductModelName NVARCHAR(100)
-);
-
-INSERT INTO #ProductTmp ([ProductID], [ProductName], [ProductModelID], [ProductModelName])
+INSERT INTO [dbo].[#ProductTmp] ([ProductID], [ProductName], [ProductModelID], [ProductModelName])
   SELECT
     xc.value('@ID', 'int'),
-    xc.value('Name[1]', 'nvarchar(100)'),
+    xc.value('Name[1]', 'nvarchar(50)'),
     xc.value('Model[1]/@ID', 'int'),
-    xc.value('Model[1]/Name[1]', 'nvarchar(100)')
+    xc.value('Model[1]/Name[1]', 'nvarchar(50)')
   FROM @xml_var_Product.nodes('//Product') XmlData(xc);
 
-DROP TABLE #ProductTmp;
+SELECT * FROM [dbo].[#ProductTmp];
 
--------------
---lab_7_option_8
 
-DECLARE @xml_var_Address XML
-SET @xml_var_Address =
+-- 8
+
+
+SELECT
+	[a].[AddressID]          AS '@ID',
+	[a].[City]               AS 'City',
+	[sp].[StateProvinceID]   AS 'Province/@ID',
+	[sp].[CountryRegionCode] AS 'Province/Region'
+FROM [Person].[Address] AS [a]
+JOIN [Person].[StateProvince] AS [sp]
+    ON [a].[StateProvinceID] = [sp].[StateProvinceID]
+FOR XML PATH ('Address'), ROOT ('Addresses')
+
+IF OBJECT_ID('[dbo].[GetAddressesFromXml]', 'P') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetAddressesFromXml];
+GO
+
+CREATE PROCEDURE [dbo].[GetAddressesFromXml]
+    @xml_var_A XML
+AS
+BEGIN
+	SELECT
+		xc.value('@ID', 'int')                             AS [AddressID],
+		xc.value('City[1]', 'nvarchar(100)')               AS [AddressCity],
+		xc.value('Province[1]/@ID', 'int')                 AS [ProvinceID],
+		xc.value('Province[1]/Region[1]', 'nvarchar(100)') AS [ProvinceRegion]
+	FROM @xml_var_A.nodes('//Address') XmlData(xc)
+END;
+GO
+
+DECLARE @xml_var_Address XML =
 (
   SELECT
-    A.[AddressID]          AS '@ID',
-    A.[City]               AS 'City',
-    SP.[StateProvinceID]   AS 'Province/@ID',
-    SP.[CountryRegionCode] AS 'Province/Region'
-  FROM [Person].[Address] AS A
-    JOIN [Person].[StateProvince] AS SP
-      ON A.[StateProvinceID] = SP.[StateProvinceID]
+    [a].[AddressID]          AS '@ID',
+    [a].[City]               AS 'City',
+    [sp].[StateProvinceID]   AS 'Province/@ID',
+    [sp].[CountryRegionCode] AS 'Province/Region'
+  FROM [Person].[Address] AS [a]
+    JOIN [Person].[StateProvince] AS [sp]
+      ON [a].[StateProvinceID] = [sp].[StateProvinceID]
   FOR XML PATH ('Address'), ROOT ('Addresses')
 );
 
-CREATE PROCEDURE ShowAddressXml
-    @xml_var_A XML
-AS
-  BEGIN
-    SELECT
-      xc.value('@ID', 'int')                             AS AddressID,
-      xc.value('City[1]', 'nvarchar(100)')               AS AddressCity,
-      xc.value('Province[1]/@ID', 'int')                 AS ProvinceID,
-      xc.value('Province[1]/Region[1]', 'nvarchar(100)') AS ProvinceRegion
-    FROM @xml_var_A.nodes('//Address') XmlData(xc)
-  END;
+EXECUTE [dbo].[GetAddressesFromXml] @xml_var_Address;
 
-EXECUTE [ShowAddressXml] @xml_var_Address;
 
-DROP PROCEDURE ShowAddressXml;
+-- 9
 
--------------
---lab_7_option_9
 
-DECLARE @xml_var_Transaction XML
-SET @xml_var_Transaction =
+IF OBJECT_ID('[tempdb].[dbo].[#DepartmentTmp]', 'U') IS NOT NULL
+	DROP TABLE [dbo].[#DepartmentTmp]
+GO
+
+DECLARE @xml_var_Transaction XML = 
 (
   SELECT
-    EDH.[StartDate] AS 'Start',
-    EDH.[EndDate]   AS 'End',
-    D.[GroupName]   AS 'Department/Group',
-    D.[Name]        AS 'Department/Name'
-  FROM [HumanResources].[EmployeeDepartmentHistory] AS EDH
-    JOIN [HumanResources].[Department] AS D
-      ON EDH.[DepartmentID] = D.[DepartmentID]
+    [edh].[StartDate] AS 'Start',
+    [edh].[EndDate]   AS 'End',
+    [d].[GroupName]   AS 'Department/Group',
+    [d].[Name]        AS 'Department/Name'
+  FROM [HumanResources].[EmployeeDepartmentHistory] AS [edh]
+    JOIN [HumanResources].[Department] AS [d]
+      ON [edh].[DepartmentID] = [d].[DepartmentID]
   FOR XML PATH ('Transaction'), ROOT ('History')
 );
 
-CREATE TABLE #TransactionTmp
+CREATE TABLE [dbo].[#DepartmentTmp]
 (
-  DepartmentSql XML
+  [DepartmentXml] XML
 );
 
-INSERT INTO #TransactionTmp ([DepartmentSql])
+INSERT INTO [dbo].[#DepartmentTmp] ([DepartmentXml])
   SELECT xc.query('Department')
   FROM @xml_var_Transaction.nodes('//Transaction') XmlData(xc);
 
-DROP TABLE #TransactionTmp;
+SELECT * FROM [dbo].[#DepartmentTmp]
 
--------------
---lab_7_option_10
 
-DECLARE @xml_var_Person XML
-SET @xml_var_Person =
-(
-  SELECT TOP 100
-    Per.[FirstName],
-    Per.[LastName],
-    Pas.[ModifiedDate]     AS 'Password/Date',
-    Pas.[BusinessEntityID] AS 'Password/ID'
-  FROM [Person].[Person] AS Per
-    JOIN [Person].[Password] AS Pas
-      ON Per.[BusinessEntityID] = Pas.[BusinessEntityID]
-  FOR XML PATH ('Person'), ROOT ('Persons')
-);
+-- 10
 
-CREATE PROCEDURE ShowPersonXml
+
+SELECT TOP 100
+    [per].[FirstName],
+    [per].[LastName],
+    [pas].[ModifiedDate]     AS 'Password/Date',
+    [pas].[BusinessEntityID] AS 'Password/ID'
+FROM [Person].[Person] AS [per]
+JOIN [Person].[Password] AS [pas]
+    ON [per].[BusinessEntityID] = [pas].[BusinessEntityID]
+FOR XML PATH ('Person'), ROOT ('Persons')
+
+IF OBJECT_ID('[dbo].[GetPersonsFromXml]', 'P') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetPersonsFromXml];
+GO
+
+CREATE PROCEDURE [dbo].[GetPersonsFromXml]
     @xml_var_P XML
 AS
   BEGIN
-    SELECT xc.query('Password') AS 'sql'
+    SELECT xc.query('Password') AS 'xml'
     FROM @xml_var_P.nodes('//Person') XmlData(xc)
   END;
+GO
 
-EXECUTE [ShowPersonXml] @xml_var_Person;
+DECLARE @xml_var_Person XML =
+(
+  SELECT TOP 100
+    [per].[FirstName],
+    [per].[LastName],
+    [pas].[ModifiedDate]     AS 'Password/Date',
+    [pas].[BusinessEntityID] AS 'Password/ID'
+  FROM [Person].[Person] AS [per]
+    JOIN [Person].[Password] AS [pas]
+      ON [per].[BusinessEntityID] = [pas].[BusinessEntityID]
+  FOR XML PATH ('Person'), ROOT ('Persons')
+);
 
-DROP PROCEDURE ShowPersonXml;
+EXECUTE [dbo].[GetPersonsFromXml] @xml_var_Person;
